b6d96e4910cd735d35d5871370d98d8da34da349
Doreen9 <doreen.dodoo@turntabl.io>
2020-01-09 16:42:54 +0000

    Added travis.yml file

--- src/test/java/io/turntabl/employementprofilingsystem/EmployementProfilingSystemApplicationTests.java
+++ src/test/java/io/turntabl/employementprofilingsystem/EmployementProfilingSystemApplicationTests.java
@@ -1,13 +1,15 @@
 package io.turntabl.employementprofilingsystem;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EmployementProfilingSystemApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void addEmployee() {
+	}
+
+	@Test
+	void getAllEmployeeProfile() {
 	}
 
 }
