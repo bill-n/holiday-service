f2bba9f7e867719b2cb78c38b101089b07b321a6
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2020-02-24 16:24:20 +0000

    Sending mail services

---
+++ db/holiday.sql
@@ -0,0 +1,30 @@
+--- DATABASE FOR HOLIDAY ME ---
+
+DROP TABLE IF EXISTS requests cascade;
+DROP TABLE IF EXISTS request_status cascade;
+
+--- REQUEST STATUS ---
+
+CREATE TABLE request_status(
+    request_status_id serial primary key NOT NULL,
+    req_status varchar(10)
+);
+
+INSERT INTO request_status(req_status) VALUES ('PENDING');
+INSERT INTO request_status(req_status) VALUES ('DECLINED');
+INSERT INTO request_status(req_status) VALUES ('APPROVED');
+
+--- REQUESTS TABLE ---
+
+CREATE TABLE requests(
+    request_id serial primary key NOT NULL,
+    requester_id int references employee(employee_id) NOT NULL,
+    request_start_date date NOT NULL,
+    request_report_date date NOT NULL,
+    request_status_id int references request_status(request_status_id) NOT NULL default 1
+);
+
+INSERT INTO request_status(req_status) VALUES ('PENDING');
+INSERT INTO request_status(req_status) VALUES ('DECLINED');
+INSERT INTO request_status(req_status) VALUES ('APPROVED');
+
