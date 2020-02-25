ec6fa0f500bdf18d654031f819cbedc30a0112ca
bill-n <teebill15@gmail.com>
2020-02-25 11:07:14 +0000

    mofified

---
+++ db/holiday/requestsTableInsertion.sql
@@ -0,0 +1,3 @@
+INSERT INTO requests(requester_id, request_start_date, request_report_date, request_status_id) values(1,'2020-02-20', '2020-02-25', 2);
+INSERT INTO requests(requester_id, request_start_date, request_report_date, request_status_id) values(1,'2020-02-20', '2020-02-25', 3);
+INSERT INTO requests(requester_id, request_start_date, request_report_date, request_status_id) values(1,'2020-02-20', '2020-02-25', 1);
