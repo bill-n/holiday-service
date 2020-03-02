caea8f4b69be585803eefe3fae2200680b47ed4e
pkerbynn <john.erbynn@gmail.com>
2020-03-02 17:56:05 +0000

    [issue]: first commit

--- src/main/java/io/turntabl/employementprofilingsystem/EmployementProfilingSystemApplication.java
+++ src/main/java/io/turntabl/employementprofilingsystem/EmployementProfilingSystemApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import io.jaegertracing.Configuration;
+import org.springframework.context.annotation.Bean;
 
 @ComponentScan(basePackages = "io.turntabl.*")
 @EnableSwagger2
@@ -18,6 +20,13 @@ public class EmployementProfilingSystemApplication {
 
 		SpringApplication.run(EmployementProfilingSystemApplication.class, args);
 
+	}
+
+	@Bean
+	public io.opentracing.Tracer initTracer() {
+		Configuration.SamplerConfiguration samplerConfig = new Configuration.SamplerConfiguration().fromEnv();
+		Configuration.ReporterConfiguration reporterConfig = new Configuration.ReporterConfiguration().fromEnv();
+		return new Configuration("EmployeeProfilingService").withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
 	}
 
 }
