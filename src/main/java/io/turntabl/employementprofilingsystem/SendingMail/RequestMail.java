6c0205e410bb32a6bb12b41b5ff4b524804df644
bill-n <teebill15@gmail.com>
2020-04-09 15:53:36 +0000

    refactored some code .

--- src/main/java/io/turntabl/employementprofilingsystem/SendingMail/RequestMail.java
+++ src/main/java/io/turntabl/employementprofilingsystem/SendingMail/RequestMail.java
@@ -104,7 +104,7 @@ public class RequestMail{
                 "            <tr>\n" +
                 "              <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top;\">\n" +
                 "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Hi there,</p>\n" +
-                "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Holiday request by " + requester_name + "</p>\n" +
+                "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Holiday request by " + from + "</p>\n" +
                 "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Start date: " + requester_start_date + "</p>\n" +
                 "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Report date: " + requester_end_date + "</p>\n" +
                 "              <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"btn btn-primary\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;\">\n" +
