6bd94a437c9539765c67d7aef5a416511d98350e
bill-n <teebill15@gmail.com>
2020-04-10 07:36:33 +0000

    modified approver url

--- src/main/java/io/turntabl/employementprofilingsystem/SendingMail/RequesterMail.java
+++ src/main/java/io/turntabl/employementprofilingsystem/SendingMail/RequesterMail.java
@@ -114,7 +114,7 @@ public class RequesterMail {
                 "                  <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;\">\n" +
                 "                    <tbody>\n" +
                 "                      <tr>\n" +
-                "                      <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;\"> <a href = \"https://holiday-request.herokuapp.com\" target=\"_blank\" style=\"display: inline-block; color: #ffffff; background-color: green; border: solid 1px green; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: green;\"> Take Action </a> </td>\n" +
+                "                      <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;\"> <a href = \"https://holiday-request.herokuapp.com/approver\" target=\"_blank\" style=\"display: inline-block; color: #ffffff; background-color: green; border: solid 1px green; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: green;\"> Take Action </a> </td>\n" +
                 "                    </tr>\n" +
                 "                    </tbody>\n" +
                 "                  </table>\n" +
