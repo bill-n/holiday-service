46dd9c02dbb8577345dcf8346b5ae2f6c3656dc6
bill-n <teebill15@gmail.com>
2020-04-24 01:54:43 +0000

    refactored recipient mail

--- src/main/java/io/turntabl/employementprofilingsystem/SendingMail/ApproverMail.java
+++ src/main/java/io/turntabl/employementprofilingsystem/SendingMail/ApproverMail.java
@@ -250,7 +250,7 @@ public class ApproverMail {
                 "            <tr>\n" +
                 "              <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top;\">\n" +
                 "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Hi there, your request was approved </p>\n" +
-                "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Holiday request by " + from + "</p>\n" +
+                "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Holiday request by " + to + "</p>\n" +
                 "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Start date: " + requester_start_date + "</p>\n" +
                 "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Report date: " + requester_end_date + "</p>\n" +
                 "              <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"btn btn-primary\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;\">\n" +
@@ -396,7 +396,7 @@ public class ApproverMail {
                 "            <tr>\n" +
                 "              <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top;\">\n" +
                 "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Hi there, your holiday request was declined </p>\n" +
-                "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Holiday request by " + from + "</p>\n" +
+                "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Holiday request by " + to + "</p>\n" +
                 "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Start date: " + requester_start_date + "</p>\n" +
                 "              <p style=\"font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px; color: black;\">Report date: " + requester_end_date + "</p>\n" +
                 "              <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"btn btn-primary\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;\">\n" +
